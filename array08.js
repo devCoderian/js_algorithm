a9c188a81445b4276d2c7d1a88a2942a74791ed8
devCoderian <devCoderian@gmail.com>
2021-10-10 03:09:41 +0900

    finish array

--- array08.js
+++ array08.js
@@ -11,9 +11,9 @@ const sol = (s, e) =>{
         num = i;
         //2.n -> %/ 
         while(num !== 0){
-            result[num % 10]++;
+            result[num % 10]++; //10의 나머지 1의 자리를 알 수 있음
             num /= 10;
-            num = parseInt(num);
+            num = parseInt(num); //나머지 정수화 나머지 연산자 //0이 될때가지 수행
         }
     }
 
